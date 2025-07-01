5faca62eff143de7fdd4338d637f313727cc65db
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-07-01 17:32:30 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/images',[ImageController::class,'getImage']);
+Route::post('/uploadImageApi',[ImageController::class,'uploadImageApi']);
