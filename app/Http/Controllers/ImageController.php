a4329dabe1bdf047afa963359c21c628df19869d
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-07-03 16:26:05 +0530

    update

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
-use App\Services\ImageService;
+use App\services\ImageService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
